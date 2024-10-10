f910b3bb4006a92c662d026cfa1e7f26a3476e7b
Peacefulhat <peacefulhat@gmail.com>
2024-10-10 07:01:24 +0530

    addons

---
+++ install_pkg.sh
@@ -0,0 +1,14 @@
+#!/bin/zsh
+#simple package installer
+file_path=$1
+
+while read -r line; do
+  echo "Checking package: $line"
+  if pacman -Q "$line" &> /dev/null; then
+      echo "$line is already installed."
+  else
+      echo "$line is not installed. Installing..."
+      sudo pacman -S --noconfirm "$line"
+  fi
+done < "$file_path"
+
