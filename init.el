fcafcc831b15fb4fe00209a89f7715692d363aed
Peacefulhat <peacefulhat@gmail.com>
2024-10-22 16:10:44 +0530

    some minor changes

--- init.el
+++ init.el
@@ -1,16 +1,20 @@
-(tool-bar-mode 0);; disable tool bar
-(menu-bar-mode 0);; disable menu bar
-(scroll-bar-mode 0);; disable-scroll-bar
-(global-display-line-numbers-mode);; enable line numbers
-(ido-mode 1);;enable ido mode
-(ido-everywhere 1);; enable ido mode
-(set-face-attribute 'default nil :height 140);; changing font size
+(tool-bar-mode 0)
+(menu-bar-mode 0)
+(scroll-bar-mode 0)
+(global-display-line-numbers-mode)
+(ido-mode 1)
+(ido-everywhere 1)
+(set-face-attribute 'default nil :height 170)
+;; Disable backup files
+(setq make-backup-files nil)
+(setq auto-save-default nil)
 ;(column-number-mode (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
-
+ '(custom-enabled-themes '(gruber-darker))
+ '(package-selected-packages '(gruber-darker-theme))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -22,15 +26,13 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
-
-;; install gruber-darker theme
  '(custom-enabled-themes '(gruber-darker))
  '(custom-safe-themes
    '("e13beeb34b932f309fb2c360a04a460821ca99fe58f69e65557d6c1b10ba18c7" default))
- '(inhibit-startup-screen t) ;; remove splash screen
- '(package-selected-packages '(gruber-darker-theme)))
- '(display-line-numbers-type 'relative);;relative line number
-;; download smex from git hub
+ '(display-line-numbers-type 'relative)
+ '(inhibit-startup-screen t)
+ '(package-selected-packages '(multiple-cursors magit gruber-darker-theme)))
+'(display-line-numbers-type 'relative)
 (load "~/smex/smex.el")
 (smex-initialize)
 (global-set-key (kbd "M-x") 'smex)
